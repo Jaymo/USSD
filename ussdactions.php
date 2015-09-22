8282d0e325d6fe0a0f962ded2e6a357a3142b21a
Jaymo <wahomey@gmail.com>
2015-09-22 11:47:06 +0300

    Coommit of core classes

---
+++ ussdactions.php
@@ -0,0 +1,23 @@
+<?php
+
+return array(
+    'My USSD App',
+    array(
+        'One Title',
+        'One.Two',
+        'One.Three',
+        array(
+            'One.Four.One',
+            'One.Four.Two',
+            'One.Four.Three',
+        ),
+    ),
+    array(
+        'Two Title',
+        'Two.One',
+        'Two.Two',
+        'Two.Three',
+    ),
+    'Three',
+    'Four',
+);
